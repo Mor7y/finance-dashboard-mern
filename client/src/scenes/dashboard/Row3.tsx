d862f52affce512a5b810a1af0954a1504ad7acb
Anatolie Gheorghilas <gheorghilas.anatolie@gmail.com>
2023-06-09 16:44:53 +0300

    Finished and rendered last dashboardBox 'j' from Row3

--- client/src/scenes/dashboard/Row3.tsx
+++ client/src/scenes/dashboard/Row3.tsx
@@ -176,7 +176,32 @@ const Row3 = () => {
                     ))}
                 </FlexBetween>
             </DashboardBox>
-            <DashboardBox gridArea="j"></DashboardBox>
+            <DashboardBox gridArea="j">
+                <BoxHeader
+                    title="Overall Summary and Explanation Data"
+                    sideText="+15%"
+                />
+                <Box
+                    height="15px"
+                    margin="1.25rem 1rem 0.4rem 1rem"
+                    bgcolor={palette.primary[800]}
+                    borderRadius="1rem"
+                >
+                    <Box
+                        height="15px"
+                        bgcolor={palette.primary[600]}
+                        borderRadius="1rem"
+                        width="40%"
+                    ></Box>
+                </Box>
+                <Typography variant="h6" margin="0 1rem">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Temporibus nostrum impedit, sapiente maxime cupiditate saepe
+                    mollitia. Iste autem assumenda id labore, perspiciatis
+                    tempora deserunt voluptatem esse quo. Totam sed voluptate
+                    non eum. Repudiandae, voluptate pariatur.
+                </Typography>
+            </DashboardBox>
         </>
     );
 };
